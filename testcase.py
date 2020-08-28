be2f9c4c7d324c75b959cf89e03e4eca8e3a3283
jly1116 <58168938+jly1116@users.noreply.github.com>
2020-08-28 22:42:28 +0800

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -1,7 +1,7 @@
 import cbapi
 
 if __name__ == "__main__":
-    search_org = {'name': 'Data',"locations": 'California'}
+    search_org = {'name': 'Tech',"locations": 'New York'}
     search_people = {'name': 'Nick', "locations": "New York"}
     result_org = cbapi.get_data(search_org, True)           # retrieve organization data
     result_people = cbapi.get_data(search_people, False)    # retrieve people data
