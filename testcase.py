f716935f42e9bb02fa6bdfa047e8c7eccb25bae4
jly1116 <58168938+jly1116@users.noreply.github.com>
2020-08-28 21:38:43 +0800

    Add files via upload

---
+++ testcase.py
@@ -0,0 +1,8 @@
+import cbapi
+
+if __name__ == "__main__":
+    search_org = {'name': 'Data',"locations": 'California'}
+    search_people = {'name': 'Nick', "locations": "New York"}
+    result_org = cbapi.get_data(search_org, True)           # retrieve organization data
+    result_people = cbapi.get_data(search_people, False)    # retrieve people data
+    result_mod = cbapi.change_timestamp(result_org)         # change timestamp to local standard time
